bd2112b0919727b1b96ff86bda69a684d30494ca
Miguel Angel Dallanegra Vilches <mdallanegra@icloud.com>
2024-03-07 21:03:36 -0300

    commit

--- HotelWise/HotelWiseWeb/home/urls.py
+++ HotelWise/HotelWiseWeb/home/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('acerca_de', views.acerca_de, name='acerca_de'),
+    path('contacto', views.contacto, name='contacto'),
+    path('dashboard', RedirectView.as_view(
+        url='https://lookerstudio.google.com/s/sfapHivroXI'), name='dashboard'),
+    path('github', RedirectView.as_view(
+        url='https://github.com/HotelWise/HotelWise'), name='github'),
+    path('etl', RedirectView.as_view(
+        url='https://github.com/HotelWise/HotelWise/tree/ELT-Google'), name='etl'),
+    path('d_analytics', RedirectView.as_view(
+        url='https://github.com/HotelWise/HotelWise/tree/HotelWise_DA'), name='d_analytics'),
+    path('m_learning', RedirectView.as_view(
+        url='https://github.com/HotelWise/HotelWise/tree/HotelWiseML'), name='m_learning'),
+    path('web_design', RedirectView.as_view(
+        url='https://github.com/HotelWise/HotelWise/tree/HotelWiseWeb'), name='web_design'),
+    path('seguridad', RedirectView.as_view(
+        url='https://github.com/HotelWise/HotelWise/tree/HotelWiseML/HotelWise/Crime_In_The_USA'), name='seguridad'),
 ]
--- HotelWise/HotelWiseWeb/home/views.py
+++ HotelWise/HotelWiseWeb/home/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'home.html')
+
+
+def acerca_de(request):
+    return render(request, 'acerca_de.html')
+
+
+def contacto(request):
+    return render(request, 'contacto.html')
