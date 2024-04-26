4f5b81eb93c5c6970239668dd4e2d9bdca9fc223
Miguel Angel Dallanegra Vilches <mdallanegra@icloud.com>
2024-04-26 13:27:05 -0300

    Update settings.py

--- HotelWise/HotelWiseWeb/HotelWise/settings.py
+++ HotelWise/HotelWiseWeb/HotelWise/settings.py
@@ -18,9 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g-ezjlgj-%+#4dex66^cp5@*!3+4vlgrcj*=%epmmll)fhvysf'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
